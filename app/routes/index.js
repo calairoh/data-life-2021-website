347cefc09ced4185e69c74b3083c85bb2385a355
Davide Calabrò <davide.calabro@mail.polimi.it>
2021-07-14 18:30:37 +0200

    Add routes for the questions pages

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET questions */
+router.get('/question-1', function(req, res, next) {
+  res.render('questions/question-1', { title: 'Question 1' });
+});
+
+router.get('/question-2', function(req, res, next) {
+  res.render('questions/question-2', { title: 'Question 2' });
+});
+
+router.get('/question-3', function(req, res, next) {
+  res.render('questions/question-3', { title: 'Question 3' });
+});
+
+router.get('/question-4', function(req, res, next) {
+  res.render('questions/question-4', { title: 'Question 4' });
+});
+
 module.exports = router;
