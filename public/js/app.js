774984ed7a39beb855f0ad8a58576fa4e1d35790
Davide Calabrò <davide.calabro@mail.polimi.it>
2021-07-30 11:48:45 +0200

    Add js for open/close appendices

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     initNavbarScroll();
+    initAppendixShowHide();
 });
 
 /**
@@ -11,6 +12,28 @@ function initNavbarScroll(){
             $('ol.nav-ol').removeClass('closed');
         } else {
             $('ol.nav-ol').addClass('closed');
+        }
+    });
+}
+
+/**
+ * Initialize the show and hide of the appendix when the title is clicked
+ */
+function initAppendixShowHide(){
+    $('.appendix-title').click(function(){
+        var $parent = $(this).parent();
+        var $body = $parent.find('.appendix-body');
+        var $arrowup = $parent.find('.arrow-up')
+        var $arrowdown = $parent.find('.arrow-down')
+
+        if(!$body.hasClass('hidden')){
+            $body.addClass('hidden');
+            $arrowup.addClass('hidden');
+            $arrowdown.removeClass('hidden');
+        } else {
+            $body.removeClass('hidden');
+            $arrowup.removeClass('hidden');
+            $arrowdown.addClass('hidden');
         }
     });
 }
