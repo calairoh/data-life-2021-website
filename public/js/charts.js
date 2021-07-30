ff64b25a4c3eb9aafeef514025254bc1e2462b45
Davide Calabrò <davide.calabro@mail.polimi.it>
2021-07-30 16:14:07 +0200

    Add draft of svg handling + pointer cursor on appendix title

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,40 +1,14 @@
 $(document).ready(function(){
-    //initCovidCasesDonationChart();
+    initChart1Legend();
 });
 
-function initCovidCasesDonationChart(){
-    const chart = raw.chart;
-    const bubblechart = rawcharts.bubblechart;
+function initChart1Legend(){
+    $('#legendNYIG').click(function(){
+        var color = $(this).attr('fill');
+        var $svg = $(this).parent().parent();
 
-
-    // defining some data.
-    const userData = [
-        { size: 10, price: 2, cat: "a" },
-        { size: 12, price: 1.2, cat: "a" },
-        { size: 1.3,price: 2, cat: "b" },
-        { size: 1.5,price: 2.2, cat: "c" },
-        { size: 10, price: 4.2, cat: "b" },
-        { size: 10, price: 6.2, cat: "c" },
-        { size: 12, price: 2.2, cat: "b" },
-    ]
-
-    // getting the target HTML node
-    const root = document.getElementById("covid-cases-donation-chart")
-
-
-    // define a mapping between dataset and the visual model
-    const mapping = {
-        x: { value: "size" },
-        y: { value: "price" },
-        color: { value: "cat" },
-    }
-
-    //instantiating the chart
-    const viz = chart(bubblechart, {
-    data: userData,
-    mapping,
-    })
-
-    //rendering into the HTML node
-    viz.renderToDOM(root)
+        $svg.find("path[fill='"+color+"']").each(function(){ 
+            $(this).addClass('hidden'); 
+        });
+    });
 }
