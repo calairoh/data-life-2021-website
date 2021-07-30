8afd029a3b8883517b59b0e34466e8e2da342553
Davide Calabrò <davide.calabro@mail.polimi.it>
2021-07-30 16:24:02 +0200

    Add problem statement file

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,6 @@
 $(document).ready(function(){
-    initNavbarScroll();
     initAppendixShowHide();
 });
-
-/**
- * Initialize the scoll of the navbar when the click event is triggered 
- */
-function initNavbarScroll(){
-    $('.navigation-handler').click(function(){
-        if($('ol.nav-ol').hasClass('closed')){
-            $('ol.nav-ol').removeClass('closed');
-        } else {
-            $('ol.nav-ol').addClass('closed');
-        }
-    });
-}
 
 /**
  * Initialize the show and hide of the appendix when the title is clicked
